0801e6b9f8567182bcc82cd3a8cc92df479b60b8
Anuka <anukazan@gmail.com>
2022-04-04 20:43:08 -0700

    lesson 5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+//no quotation marks necessary
+
+console.log(playerName); //shows up in Console tab in Inspect
+
+function fight(){
+    window.alert ("The fight has begun!");
+} //function named "fight"
+
+
+//fight();
