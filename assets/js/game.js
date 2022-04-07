23fdeec48b260793657c6f2e14670d3ade7d8dfa
Anuka <anukazan@gmail.com>
2022-04-06 21:13:48 -0700

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -53,12 +53,20 @@ var fight = function(enemyName){
     }   
 }
 
-for (var i=0; i<enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth= 50;
-    fight(pickedEnemyName);
-};
+for (i=0; i<enemyNames.length; i++){
+    if (playerHealth>0){
+        window.alert("Welcome to Robot Gladiators! Round " + (i+1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth= 50;
+        //debugger;
+        fight(pickedEnemyName);
 
+    } else{
+        window.alert("You have lost your robot in battle! Game over!");
+        break;
+    }
+
+}
 
 //game states
 // * defeat each enemy robot
