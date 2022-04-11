696a27c6292b3fc375f333c22bee7e10cc1f8ab8
Anuka <anukazan@gmail.com>
2022-04-11 00:30:26 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,13 +17,14 @@ var fight = function(enemyName){
 
             if (confirmSkip){
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                playerMoney = playerMoney -10;
+                playerMoney = Math.max(0, playerMoney-10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
         
-        enemyHealth=enemyHealth-playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth=Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " +enemyHealth + " health remaining."
         );
@@ -37,7 +38,8 @@ var fight = function(enemyName){
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
-        playerHealth=playerHealth-enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth= Math.max(0, playerHealth-damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -61,7 +63,7 @@ var startGame=function(){
          if (playerHealth>0){
              window.alert("Welcome to Robot Gladiators! Round " + (i+1));
              var pickedEnemyName = enemyNames[i];
-             enemyHealth= 50;
+             enemyHealth= randomNumber(40, 60);
              //debugger;
              fight(pickedEnemyName);
              if (playerHealth > 0 && i < enemyNames.length-1){
@@ -145,6 +147,10 @@ var shop = function(){
     }
 };
 
+var randomNumber = function(min, max){
+    var value = Math.floor(Math.random() * (max - min +1) + min);
+    return value;
+}
 
 startGame();
 
